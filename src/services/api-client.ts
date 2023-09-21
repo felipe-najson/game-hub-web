469afbe219bca41b048aa77b2644d1bee68401ff
Felipe Najson <felipe.najson@gmail.com>
2023-09-21 12:11:18 -0300

    hide rawg key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,36 +1,36 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios'
 
 export interface FetchResponse<T> {
-  count: number;
-  next: string | null;
-  results: T[];
+  count: number
+  next: string | null
+  results: T[]
 }
 
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '16513ce8b0234753a9c51c8b2ae8ab76',
-  },
-});
+    key: import.meta.env.VITE_RAWG_KEY
+  }
+})
 
 class APIClient<T> {
-  endpoint: string;
+  endpoint: string
 
   constructor(endpoint: string) {
-    this.endpoint = endpoint;
+    this.endpoint = endpoint
   }
 
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
       .get<FetchResponse<T>>(this.endpoint, config)
-      .then((res) => res.data);
-  };
+      .then((res) => res.data)
+  }
 
   get = (id: number | string) => {
     return axiosInstance
       .get<T>(this.endpoint + '/' + id)
-      .then((res) => res.data);
-  };
+      .then((res) => res.data)
+  }
 }
 
-export default APIClient;
+export default APIClient
